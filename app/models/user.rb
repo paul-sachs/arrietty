ba659de39000f3b994c3447b6c2e0cd9651d2c19
Uzo Ogbue <uzo@influitive.com>
2016-02-22 21:15:22 -0500

    Added validation to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+	validates :email, uniqueness: true,
+			  		  presence: true,
+			  		  email: true
+  	validates :first_name, presence: true
+
+  	validates :last_name, presence: true
+
+  	validates :phone, format: { with: /\(\d{3}\)\d{3}-\d{4}/},
+  					  presence: true
+
 end
