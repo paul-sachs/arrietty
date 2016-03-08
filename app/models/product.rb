f169aa233aeb1b8da37a735c45ca3178898d57fe
Paul Sachs <dev+paul@influitive.com>
2016-03-08 18:55:25 -0500

    Added image attachment gem for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :category
-  # belongs_to :user
+  belongs_to :user
+
+  has_attached_file :image
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308015717) do
+ActiveRecord::Schema.define(version: 20160308233420) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,8 +24,12 @@ ActiveRecord::Schema.define(version: 20160308015717) do
     t.integer  "user_id"
     t.integer  "category_id"
     t.float    "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
