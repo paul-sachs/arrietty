3099866d092500c185104b33c5e51e7e89c358f2
Paul Sachs <paul.sachs@influitive.com>
2016-03-22 18:44:15 -0400

    Add header nav bar.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   resources :products
 
+  get '/user_preferences' => 'users#preferences'
+
   devise_scope :user do
     match '/twitter_sign_up' =>
         'users/omniauth_callbacks#twitter_submit', via: [:post, :patch]
