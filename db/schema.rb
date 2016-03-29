b67ee0a67df7411d87a49325f16bdc2ac7e0b85c
Paul Sachs <paul.sachs@influitive.com>
2016-03-28 21:07:57 -0400

    Added some style

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323213630) do
+ActiveRecord::Schema.define(version: 20160328225121) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,8 @@ ActiveRecord::Schema.define(version: 20160323213630) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "token"
+    t.string   "secret"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
